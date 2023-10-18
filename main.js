485a83e8ec7f3670e17f52bcea35492854ab98a7
DamianoL <damianolunardo@hotmail.it>
2023-10-18 16:05:02 +0200

    ADD bonus 1

--- main.js
+++ main.js
@@ -14,37 +14,41 @@ const teamMembers = [
     {
         nome_cognome: 'Walter Gordon',
         ruolo: 'Office Manager',
-        immagine: './img/walter-gordon-office-manager-founder-ceo.jpg',
+        immagine: './img/walter-gordon-office-manager.jpg',
     },
     {
         nome_cognome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
-        immagine: './img/angela-lopez-social-media-founder-ceo.jpg',
+        immagine: './img/angela-lopez-social-media-manager.jpg',
     },
     {
         nome_cognome: 'Scott Estrada',
         ruolo: 'Developer',
-        immagine: './img/scott-estrada-developer-founder-ceo.jpg',
+        immagine: './img/scott-estrada-developer.jpg',
     },
     {
         nome_cognome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
-        immagine: './img/barbara-ramos-graphic-designer-ceo.jpg',
+        immagine: './img/barbara-ramos-graphic-designer.jpg',
     },
 ]
 
 //console.log(teamMembers);
 
-for (let i = 0; i < teamMembers.length; i++) {
-    console.log(teamMembers[i].nome_cognome);
-    console.log(teamMembers[i].ruolo);
-    console.log(teamMembers[i].immagine);
-};
+//for (let i = 0; i < teamMembers.length; i++) {
+    //console.log(teamMembers[i].nome_cognome);
+    //console.log(teamMembers[i].ruolo);
+    //console.log(teamMembers[i].immagine);
+//};
 
 for (let i = 0; i < teamMembers.length; i++) {
     const infoTeam = teamMembers[i]
     //console.log(infoTeam)
-    const fullInfo = `${infoTeam.nome_cognome} ${infoTeam.ruolo} ${infoTeam.immagine}`
+    const fullInfo = `
+    ${infoTeam.nome_cognome}
+    ${infoTeam.ruolo}
+    ${infoTeam.immagine}
+    `
     console.log(fullInfo)
 };
 
@@ -57,15 +61,15 @@ for (let i = 0; i < teamMembers.length; i++) {
     const nomeCognome = document.createElement('h5')
     nomeCognome.textContent = member.nome_cognome
 
-    const professione = document.createElement('p')
-    professione.textContent = member.ruolo 
+    const ruolo = document.createElement('p')
+    ruolo.textContent = member.ruolo 
     
-    const immagineProfilo = document.createElement('p')
-    immagineProfilo.textContent = member.immagine
-    
+    const immagineProfilo = document.createElement('img')
+    immagineProfilo.src = member.immagine 
+   
     membriDelTeam.appendChild(memberDiv)
     memberDiv.appendChild(nomeCognome)
-    memberDiv.appendChild(professione)
+    memberDiv.appendChild(ruolo)
     memberDiv.appendChild(immagineProfilo)
-    
+
 }
